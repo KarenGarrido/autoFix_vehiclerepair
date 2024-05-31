7cd6d2fd22d2f0170341877184a91740b8cf33b9
Karen Garrido <karen.garrido.c@usach.cl>
2024-05-31 00:31:05 -0400

    Levantamiento MS

---
+++ src/main/java/com/tingeso/vehiclerepairservice/VehiclerepairserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tingeso.vehiclerepairservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclerepairserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclerepairserviceApplication.class, args);
+	}
+
+}
